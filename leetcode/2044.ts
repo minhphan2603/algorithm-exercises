4b2c50ceb1f9ea0aa0e5df9e06c600abe0e0004c
minh.phan <minhphan2603@gmail.com>
2025-07-28 08:17:35 +0700

    refactor: optimize solution

--- leetcode/2044.ts
+++ leetcode/2044.ts
@@ -1,17 +1,14 @@
 /** https://leetcode.com/problems/count-number-of-maximum-bitwise-or-subsets/?envType=daily-question&envId=2025-07-28 */
 function countMaxOrSubsets(nums: number[]): number {
-  let maxOr = 0;
+  const maxOr = nums
+    .sort((a, b) => b - a)
+    .reduce((total, num) => total | num, 0);
   let result = 0;
 
   const greedy = (i: number, calculatingOr: number) => {
-    if (i >= nums.length) {
-      if (calculatingOr === maxOr) {
-        result++;
-      } else if (calculatingOr > maxOr) {
-        maxOr = calculatingOr;
-        result = 1;
-      }
-    } else {
+    if (calculatingOr === maxOr) {
+      result = result + Math.pow(2, Math.max(0, nums.length - i));
+    } else if (i < nums.length) {
       greedy(i + 1, calculatingOr);
       greedy(i + 1, calculatingOr | nums[i]);
     }
